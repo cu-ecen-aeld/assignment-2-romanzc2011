67bd763dec2927a7a9a36e2a0f22940fbe71e83e
romanzc2011 <romanzc2011@gmail.com>
2024-08-05 12:29:47 -0500

    Assignment 2 redo

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -48,9 +48,10 @@ then
 		exit 1
 	fi
 fi
-#echo "Removing the old writer utility and compiling as a native application"
+
+# make the writer utility
 make clean
-make
+make CROSS_COMPILE=aarch64-none-linux-gnu- 
 
 for i in $( seq 1 $NUMFILES)
 do
